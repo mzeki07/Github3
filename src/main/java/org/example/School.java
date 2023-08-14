f4ea50fa1e88335fd733b7ea76f63071d83ec359
zeki <zekiursavas07@gmail.com>
2023-08-14 18:42:17 -0400

    second commit school and teacher and main added

---
+++ src/main/java/org/example/School.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class School {
+    private String name;
+    private int studentcount;
+}
---
+++ src/main/java/org/example/Teacher.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Teacher {
+    public static void main(String[] args) {
+        System.out.println("we don't need no education");
+    }
+}
